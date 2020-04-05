b8625b19743946c19f0b3ff4fc4af65fcd188405
Samdvich <samuel.aj.page@gmail.com>
2020-04-05 10:32:23 +1000

    Summary panel prototype
    Using house 'Karle' as a template, this would be different depending on database account info. The styling isn't done yet, this is just the panel info and the header pretty much.

--- summary.php
+++ summary.php
@@ -16,7 +16,6 @@
     else
     $database_variable = "Connected";
     
-    echo $_SESSION['name'];
     if (!isset($_SESSION['name'])) {
         header("Location: index.php");
     }
@@ -28,8 +27,68 @@
         <title>CompUI Panel</title>
     </head>
     <body>
-        <form>
-            <a href='logout.php'>Logout</a>
+        <div class='header'>Karle House</div>
+        
+        <div class='info'>
+          <span id='points'>0</span>
+          <span id='members'>0</span>
+          <span id='attendance'>83%</span>
+          <span id='rank'>#0</span>
+        </div>
+        
+        <form class='logout-form'>
+          <a id='logout' href='logout.php'>Logout <?php echo $_SESSION['name']; ?></a>
         </form>
+        
+        <style>
+          @import url('https://fonts.googleapis.com/css?family=Bungee&display=swap');
+          
+          html, body {
+            display: grid;
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+          }
+          
+          body {
+            grid-template-rows: 18% 6% 30%;
+            grid-template-columns: 0; /* Add columns later? */
+          }
+          
+          .header {
+            grid-row: 1;
+            grid-column: 1 / 4;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: navy;
+            height: 100%;
+            width: 100%;
+            font-family: 'Bungee', regular;
+            font-size: 350%;
+            color: white;
+          }
+          
+          .info {
+            display: grid;
+            grid-row: 3;
+            grid-column: 1 / 4;
+            background-color: #EAEAEA;
+            grid-template-columns: 25% 25% 25% 25%;
+          }
+        
+          span {
+            all: unset;
+            display: grid;
+            text-align: center;
+            align-items: center;
+            font-size: 350%;
+          }
+          
+          .logout-form {
+            grid-row: 4;
+          }
+        </style>
     </body>
 </html>
