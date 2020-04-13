2a9c443692c5867a5db0e54db31c8ed707898608
Samdvich <samuel.aj.page@gmail.com>
2020-04-13 22:24:36 +1000

    Added generic admin pages
    The files contain basic html, no body, no styling. The pages are mainly just a head with a <title> containing the page name, a database connection and a check for the account type using PHP header.

---
+++ admin/notices.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+
+    $servername = "localhost";
+    $username = "spage65";
+    $password = "Password1";
+    $DB_Name = "spage65_CompUI"; # Using 2020 PHP Default BCRYPT Hash = password_hash($password, PASSWORD_BCRYPT)
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $DB_Name);
+     
+    // Check connection
+      if ($conn->connect_error) {
+        die("Not Connected: <br>" . $conn->connect_error);
+    }
+    else
+    $database_variable = "Connected";
+    
+    if ($_SESSION['type'] !== "admin") {
+        header("Location: ../summary.php");
+    }
+?>
+
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Accounts | Notices</title>
+  </head>
+</html>
