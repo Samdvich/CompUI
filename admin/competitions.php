87ef783395043580816dcb94a79d2861b835df29
Samdvich <samuel.aj.page@gmail.com>
2020-04-21 20:27:33 +1000

    Header home button implementation
    In extension of commit 18e92c837c38b71a246dda53332cab9704cb5af3 - Due to some form resubmission on an admin page, a previously tested home button that jumps back to the summary panel has been added (icon is from the bootstrap icon svg directory).

--- admin/competitions.php
+++ admin/competitions.php
@@ -30,7 +30,10 @@
     <title>Admin | Competitions</title>
   </head>
   <body>
-    <div class='header'>Competitions</div>
+    <div class='header'>
+      <a href='/summary.php' id='home'><svg class="bi bi-house-fill" width="4em" height="4em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 01-1.5 1.5h-9A1.5 1.5 0 012 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M7.293 1.5a1 1 0 011.414 0l6.647 6.646a.5.5 0 01-.708.708L8 2.207 1.354 8.854a.5.5 0 11-.708-.708L7.293 1.5z" clip-rule="evenodd"/></svg></a>
+      <p id='heading'>Competitions</p>
+    </div>
     <div class='overflow-table'>
     <?php
       $sql = "SELECT event FROM competitions";
@@ -99,7 +102,11 @@
       
       body { grid-template-rows: 18% 80%; grid-template-columns: 30% 50% 20%; }
       
-      .header { grid-row: 1; grid-column: 1 / 4; display: flex; justify-content: center; align-items: center; background-color: <?php echo $house_color; ?>; height: 100%; width: 100%; font-family: 'Bungee', regular; font-size: 350%; color: white;}
+      .header { grid-row: 1; grid-column: 1 / 4; background-color: <?php echo $house_color; ?>; height: 100%; width: 100%; font-family: 'Bungee', regular; display: grid; grid-template-columns: 8% auto 8%; }
+      
+      #home { grid-column: 1; color: white; margin: auto; }
+      
+      #heading { grid-column: 2; font-size: 350%; color: white; margin: auto; }
       
       .overflow-table { grid-row: 2; grid-column: 2; width: 100%; height: 75%; overflow: auto; margin: auto; white-space: nowrap; }
       
