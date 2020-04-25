3508b3b86e097d50a40848b11b8c9853ad820d9f
Samdvich <samuel.aj.page@gmail.com>
2020-04-25 13:04:24 +1000

    Basic accounts page prep
    Just added a header with the same grid and form as the competitions page.

--- admin/accounts.php
+++ admin/accounts.php
@@ -15,6 +15,13 @@
     
     if ($_SESSION['type'] !== "admin") // Authenticate
       {header("Location: ../summary.php");}
+      
+    $secure = $conn->prepare('SELECT colour, members FROM houses WHERE house = ?');
+    $secure->bind_param('s', $_SESSION['house']);
+    $secure->execute();
+    $secure->store_result();
+    $secure->bind_result($house_color, $members);
+    $secure->fetch();
 ?>
 
 <!DOCTYPE HTML>
@@ -22,4 +29,49 @@
   <head>
     <title>Accounts | Admin</title>
   </head>
+  <body>
+    <div class='header'>
+      <a href='../summary.php' id='home'><svg class="bi bi-house-fill" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 01-1.5 1.5h-9A1.5 1.5 0 012 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M7.293 1.5a1 1 0 011.414 0l6.647 6.646a.5.5 0 01-.708.708L8 2.207 1.354 8.854a.5.5 0 11-.708-.708L7.293 1.5z" clip-rule="evenodd"/></svg></a>
+      <p id='heading'><?php echo $_SESSION['house']; ?></p>
+    </div>
+    
+    <div class='new-comp'>
+        <!-- ... -->
+    </div>
+    
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Bungee&display=swap');
+      @import url('https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900&display=swap');
+      
+      @keyframes poof { from {visibility: visible;} to {visibility: hidden;} }
+      
+      html, body { display: grid; margin: 0; padding: 0; height: 100%; width: 100%; }
+      
+      body { grid-template-rows: 18% 80%; grid-template-columns: 30% 50% 20%; }
+      
+      .header { grid-row: 1; grid-column: 1 / 4; background-color: <?php echo $house_color; ?>; height: 100%; width: 100%; font-family: 'Bungee', regular; display: grid; grid-template-columns: 8% auto 8%; }
+      
+      #home { grid-column: 1; color: white; margin: auto; }
+      
+      #heading { grid-column: 2; font-size: 350%; color: white; margin: auto; }
+      
+      .competition-table { width: 100%; font-family: 'Bungee', regular; font-size: 200%; }
+      
+      .new-comp { box-sizing: border-box; padding: 5%; grid-row: 2; grid-column: 1; height: 80%; width: 80%; background-color: <?php echo $house_color ?>; margin: auto; }
+      
+      #event { font-family: 'Raleway'; font-weight: 200; }
+      
+      #event-label { display: grid; color: white; font-family: 'Raleway'; font-size: 120%; margin-left: 5%; }
+        
+      #event-name-field { display: grid; border-radius: 50px; border: 0 solid transparent; width: 90%; height: 45px; font-size: 150%; padding-left: 5%; padding-right: 5%; font-family: 'Raleway'; text-transform: capitalize; margin-top: 4%; }
+      
+      #event-button { all: unset; display: grid; font-family: Bungee; font-size: 155%; color: white; margin: auto; margin-top: 5%; }
+      
+      #event-button:hover { color: whitesmoke; }
+      
+      #event-create-button:active { font-size: 140%; }
+      
+      a { all: unset; cursor: pointer; }
+    </style>
+  </body>
 </html>
--- summary.php
+++ summary.php
@@ -54,7 +54,7 @@
   </head>
   <body>
     <div class='header'>
-      <a href='logout.php' id='home'><svg class="bi bi-box-arrow-in-left" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 000-.708L5.207 8l2.647-2.646a.5.5 0 10-.708-.708l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708 0z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M15 8a.5.5 0 00-.5-.5h-9a.5.5 0 000 1h9A.5.5 0 0015 8z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M2.5 14.5A1.5 1.5 0 011 13V3a1.5 1.5 0 011.5-1.5h8A1.5 1.5 0 0112 3v1.5a.5.5 0 01-1 0V3a.5.5 0 00-.5-.5h-8A.5.5 0 002 3v10a.5.5 0 00.5.5h8a.5.5 0 00.5-.5v-1.5a.5.5 0 011 0V13a1.5 1.5 0 01-1.5 1.5h-8z" clip-rule="evenodd"/></svg></a>
+      <a href='logout.php' id='logout'><svg class="bi bi-box-arrow-in-left" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 000-.708L5.207 8l2.647-2.646a.5.5 0 10-.708-.708l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708 0z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M15 8a.5.5 0 00-.5-.5h-9a.5.5 0 000 1h9A.5.5 0 0015 8z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M2.5 14.5A1.5 1.5 0 011 13V3a1.5 1.5 0 011.5-1.5h8A1.5 1.5 0 0112 3v1.5a.5.5 0 01-1 0V3a.5.5 0 00-.5-.5h-8A.5.5 0 002 3v10a.5.5 0 00.5.5h8a.5.5 0 00.5-.5v-1.5a.5.5 0 011 0V13a1.5 1.5 0 01-1.5 1.5h-8z" clip-rule="evenodd"/></svg></a>
       <p id='heading'><?php echo $_SESSION['house']; ?></p>
     </div>
     
@@ -90,7 +90,7 @@
           
       .header { grid-row: 1; grid-column: 1 / 4; background-color: <?php echo $house_color; ?>; height: 100%; width: 100%; font-family: 'Bungee', regular; display: grid; grid-template-columns: 8% auto 8%; }
       
-      #home { grid-column: 1; color: white; margin: auto; }
+      #logout { grid-column: 1; color: white; margin: auto; }
       
       #heading { grid-column: 2; font-size: 350%; color: white; margin: auto; }
           
