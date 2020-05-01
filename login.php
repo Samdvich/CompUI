384a120aa0e11cec08c417dd65085fb167abed09
Samdvich <samuel.aj.page@gmail.com>
2020-05-02 00:43:24 +1000

    Polishing the login page
    - Bootstrap icons added to show database connection
    - Login error message moved to under the login form (with font styling)
    - Login error message fades after a 10 second delay
    - Error messages no longer stored together to be used for debugging, now just for users

--- login.php
+++ login.php
@@ -4,14 +4,12 @@
   $password = "Password1";
   $DB_Name = "spage65_CompUI"; # Using 2020 PHP Default BCRYPT Hash = password_hash($password, PASSWORD_BCRYPT)
   
-  unset($ffs_variable);
-  
   $conn = new mysqli($servername, $username, $password, $DB_Name); # Connection
      
   if ($conn->connect_error) # Testing Connection
-    {die("Not Connected: <br>" . $conn->connect_error);}
+    {$database_status = '<svg class="bi bi-bar-chart" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5h-2v12h2V2zm-2-1a1 1 0 00-1 1v12a1 1 0 001 1h2a1 1 0 001-1V2a1 1 0 00-1-1h-2zM6 7a1 1 0 011-1h2a1 1 0 011 1v7a1 1 0 01-1 1H7a1 1 0 01-1-1V7zm-5 4a1 1 0 011-1h2a1 1 0 011 1v3a1 1 0 01-1 1H2a1 1 0 01-1-1v-3z" clip-rule="evenodd"/></svg>';}
   else
-    {$database_variable = "Connected";}
+    {$database_status = '<svg class="bi bi-bar-chart-fill" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><rect width="4" height="5" x="1" y="10" rx="1"/><rect width="4" height="9" x="6" y="6" rx="1"/><rect width="4" height="14" x="11" y="1" rx="1"/></svg>';}
 ?>
 
 <!DOCTYPE HTML>
@@ -21,8 +19,6 @@
   </head>
 
     <?php
-      $ffs_variable = "";
-      
       if ($secure = $conn->prepare('SELECT userID, house, type, password FROM accounts WHERE email = ?')) // prevent SQL injection
         {$secure->bind_param('s', $_POST['user-field']); /* s = string, i = int, b = blob */ $secure->execute(); /* run the query */ $secure->store_result();
         if ($secure->num_rows > 0) /* if there is a result */
@@ -30,16 +26,15 @@
           if (password_verify($_POST['password-field'], $password))
             {session_start(); $_SESSION['loggedin'] = TRUE; $_SESSION['name'] = $_POST['user-field']; $_SESSION['id'] = $id; $_SESSION['house'] = $house; $_SESSION['type'] = $type; header('Location: index.php');}
           else
-            {$ffs_variable = 'Error: Incorrect password';}}
+            {$ffs_variable = 'Incorrect Password';}}
         else
-          {$ffs_variable = 'Error: Unknown email';}}
+          {$ffs_variable = 'Unrecognised Email';}}
       else
-        {$ffs_variable = 'Error: Service outage';}
+        {$ffs_variable = 'Service Outage';}
     ?>
-        
-    <p class='error-info'>
-      Database Status: <?php echo $database_variable; ?><br><?php echo $ffs_variable; ?>
-    </p>
+    
+    <div class='database_status'><?php echo $database_status; ?></div>
+    <div class='ffs_variable'><?php echo $ffs_variable; ?></div>
         
     <form method='POST' class='login-form' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
       <input type='text' class='user-field' name='user-field' id='user-field' placeholder="Email" maxlength='20' method='POST'>
@@ -53,6 +48,8 @@
       @import url('https://fonts.googleapis.com/css?family=Bungee&display=swap');
             
       @keyframes boing { from {margin-top: 0;} 50% {margin-top: 2%;} to {margin-top: 0;} }
+      
+      @keyframes fade { from {opacity: 100%;} to {opacity: 0;} }
             
       html { display: grid; height: 100%; width: 100%; background-color: #B9D07F; }
             
@@ -63,9 +60,7 @@
       .login-form { display: grid; grid-column: 2; grid-row: 2; grid-template-rows: 40% 10% 40%; grid-template-columns: auto; }
             
       .login-submit { display: none; }
-            
-      .error-info { display: grid; grid-column: 1; grid-row: 1; margin: 0; }
-            
+      
       .user-label, .password-label { display: grid; grid-column: 1; margin: auto; margin-top: 0; font-family: 'Bungee'; color: white; font-size: 350%;}
             
       .user-label { grid-row: 1; }
@@ -79,6 +74,10 @@
       .user-field {grid-row: 1; width: 70%;}
             
       .password-field {grid-row: 3; width: 90%;}
+      
+      .database_status { grid-row: 1; grid-column: 1; margin: 0; padding: 3%; color: gray; }
+      
+      .ffs_variable { grid-row: 3; grid-column: 2; margin: auto; margin-top: 0; font-family: Roboto; font-weight: 600; color: gray; font-size: 120% ; animation-name: fade; animation-delay: 10s ; animation-duration: 5s; animation-iteration-count: 1; animation-fill-mode: forwards; }
     </style>
   </body>
 </html>
